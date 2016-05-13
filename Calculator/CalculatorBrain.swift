e297c236a936ff3f64239fec3820e65beed1302a
Noah Safian <birinoah@gmail.com>
2016-05-13 15:43:44 -0400

    trial pt 2

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,120 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Noah Safian on 2/2/16.
+//  Copyright © 2016 Noah Sfian. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private var opStack = [Op]()
+    
+    private var knownOps = [String: Op]()
+    
+    var delegate: CalculatorBrainDelegate?
+    
+    init() {
+        func learnOp(op: Op){
+            knownOps[op.description] = op
+        }
+        learnOp(Op.BinaryOperation("✕", *))
+        learnOp(Op.BinaryOperation("-", {$1-$0} ))
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("÷", {$1/$0} ))
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+                
+            case .BinaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let  operand = operandEvaluation.result{
+                    let operandEvaluation2 = evaluate(operandEvaluation.remainingOps)
+                    if let operand2 = operandEvaluation2.result {
+                        return (operation(operand, operand2), operandEvaluation2.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, _) = evaluate(opStack)
+        print("\(opStack)")
+        return result
+    }
+    
+    private enum Op: CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get{
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                    
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                    
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    func pushOperand(operand: Double)->Double?{
+        opStack.append(Op.Operand(operand))
+        tryUpdateDelegate()
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String)->Double? {
+        if let operation = knownOps[symbol]{
+            opStack.append(operation)
+            tryUpdateDelegate()
+            print("\(opStack)")
+            return evaluate()
+        }
+        return nil
+    }
+    
+    private func tryUpdateDelegate() {
+        if let delegate: CalculatorBrainDelegate = delegate {
+            delegate.historyUpdated()
+        }
+    }
+    
+    func getHistory() -> String {
+        return opStack.description
+    }
+    
+    func clear(){
+        opStack = [Op]()
+    }
+}
+
+protocol CalculatorBrainDelegate{
+    func historyUpdated()
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,115 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalculatorViewController: UIViewController, CalculatorBrainDelegate {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    var brain = CalculatorBrain()
+    
+    static let decimal_pt = "."
+    
+    @IBAction func clearPressed(sender: UIButton) {
+        brain.clear()
+        userIsTyping = false
+        displayValue = 0
+        historyLabel.text = ""
+    }
+    
+    @IBAction func constantPressed(sender: UIButton) {
+        let title = sender.currentTitle!
+        let value = { () -> Double in
+            switch title {
+            case "π":
+                return M_PI
+            default:
+                print("\(title) is not a known constant")
+                return 0
+            }
+        }()
+        
+        if value != 0 {
+            enter()
+            displayValue = value
+            enter()
+        }
+    }
+    
+    @IBAction func digitPressed(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsTyping{
+            // If it is not the case that the digit pressed is the decimal and one is already in our string
+            if !((digit == CalculatorViewController.decimal_pt) && (self.display.text!.rangeOfString(CalculatorViewController.decimal_pt) != nil)) {
+                
+                self.display.text = self.display.text! + digit
+            }
+            
+        }else
+        {
+            display.text = digit
+            userIsTyping = true
+        }
+        
+    }
+    @IBAction func enter() {
+        userIsTyping = false
+        
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    @IBAction func operatorPressed(sender: UIButton) {
+        if userIsTyping {
+            self.enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    func historyUpdated() {
+        historyLabel.text = brain.getHistory()
+    }
+    
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        brain.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
